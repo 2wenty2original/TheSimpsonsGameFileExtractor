5f379fe3fc6091a07d90901746d372cc196a682e
Khai Ailyan <89317960+2wenty2original@users.noreply.github.com>
2025-04-23 02:25:11 +0100

    More additions to dissertain

--- Final year final product/Final year final product/Source/BSPOpen.cpp
+++ Final year final product/Final year final product/Source/BSPOpen.cpp
@@ -66,35 +66,81 @@ void BSPOpen::ReadHeader()
 
 	int BlankNumber3 = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
 
+	// takes us to the first node :D
 	int FirstOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
-	int SecondOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
-	int ThirdOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
-	int FourthOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
-	int FifthOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
 
-	// start of the actual geometry
-	int SixthOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
+	// i believe these are the uv data, im just saying
+	int UVsOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
 
-	// end of geometry start of padding
-	int SeventhOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
+	// padding
+	int UVPadding = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
 
-	int EighthOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
+	// then these are the uv dictionary offset
+	int UVDictionary = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
+
+	// actual uvs indexes
+
+	int UVIndexes = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
+
+	// mesh data, i believe
+
+	int MeshOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
+
+	// mesh padding 
+	int MeshPadding = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
+
+	// mesh dictionary 
+
+	int MeshDictionary = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
+
+	// mesh indexes 
+
+	int MeshIndexes = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
+
+	BSP_Node_Header FirstNodeInformation = BSP_Node_Header
+	{
+		FirstNodeInformation.First = Char_Byte(Characters.begin(), FirstOffset, 4).CastTo32Float_BE(),
+		FirstNodeInformation.Second = Char_Byte(Characters.begin(), FirstOffset, 4).CastTo32Float_BE(),
+		FirstNodeInformation.Third = Char_Byte(Characters.begin(), FirstOffset, 4).CastTo32Float_BE(),
+		FirstNodeInformation.Fourth = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_BE().variable,
+		FirstNodeInformation.Fifth = Char_Byte(Characters.begin(), FirstOffset, 4).CastTo32Float_BE(),
+		FirstNodeInformation.Sixth = Char_Byte(Characters.begin(), FirstOffset, 4).CastTo32Float_BE(),
+		FirstNodeInformation.Seventh = Char_Byte(Characters.begin(), FirstOffset, 4).CastTo32Float_BE(),
+		FirstNodeInformation.Eighth = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_BE().variable,
+		FirstNodeInformation.Ninth = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_BE().variable,
+		FirstNodeInformation.Tenth = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_BE().variable,
+		FirstNodeInformation.Eleventh = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_BE().variable,
+		FirstNodeInformation.Twelfth = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_BE().variable,
+		FirstNodeInformation.Thirteen = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_BE().variable,
+		FirstNodeInformation.Fourteen = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_BE().variable,
+		FirstNodeInformation.Fifteen = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_LE().variable,
+		FirstNodeInformation.Sixteen = Char_Byte(Characters.begin(), FirstOffset, 4).CastToint32_BE().variable
+
+	};
+
+	FirstNode = new BSPNode();
+	FirstNode->Info = FirstNodeInformation;
+
+	int counter = 0;
+	int leafcounter = 0;
+
+	std::vector<BSPNode> LeafNodes;
+
+	RecursiveTreeBuilder(FirstNode, counter, leafcounter);
+
+	RecursiveLeafFinder(LeafNodes, FirstNode);
+
+	
+
+	Offset = MeshOffset;
 
 
-	int NinethOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
-
-
-	int TenthOffset = Char_Byte(Characters.begin(), Offset, 4).CastToint32_BE().variable;
-
-	Offset = SixthOffset;
-
-
-	std::vector<int> FaceIndexes;
+	std::vector<std::vector<int>> FaceIndexes;
 
 	std::vector<Vector3> VerticesTable;
 	std::vector<Vector2> UvTable;
 
-	for (int i = SixthOffset; i < SeventhOffset; i+= 16) 
+	for (int i = MeshOffset; i < MeshPadding; i+= 16)
 	{
 		float x = Char_Byte(Characters.begin(), Offset, 4).CastTo32Float_BE();
 		float y = Char_Byte(Characters.begin(), Offset, 4).CastTo32Float_BE();
@@ -103,7 +149,7 @@ void BSPOpen::ReadHeader()
 
 
 		UvTable.push_back(Vector2(0, 0));
-		UvTable.push_back(Vector2(0, 0));
+
 
 
 		Vector3 TempVert = Vector3{ x,y,z };
@@ -112,23 +158,204 @@ void BSPOpen::ReadHeader()
 
 	
 	
-	Offset = NinethOffset;
+	Offset = MeshDictionary;
 	Offset += 4;
 
-	for (int i = NinethOffset; i < TenthOffset; i+=2) {
-		int Index = Char_Byte(Characters.begin(), Offset, 2).CastToUint16_BE().variable;
-		FaceIndexes.push_back(Index);
+	int StartMeshDictionary = MeshIndexes - Offset;
+	// this is the mesh dictionary
+	for (int i = 0; i < StartMeshDictionary - 8; i+=8) {
+
+		// this number is the start of the actual index data, also move it 4 because its 8 byte blocked
+		int DictionaryOffset = Char_Byte(Characters.begin(), Offset,4).CastToint32_BE().variable;
+		Offset += 4;
+
+		// this is done so we can look for the end, without screwing it up
+		
+		int tempOffset = Offset;
+
+		// this is the actual ending, using the fake offset, so our normal offset isnt broken
+
+		int DictionaryOffsetEnding = Char_Byte(Characters.begin(), tempOffset, 4).CastToint32_BE().variable;
+
+		int TempOffset = DictionaryOffset;
+
+		std::vector<int> TriangleStripIndexes;
+
+		for (int j = DictionaryOffset; j < DictionaryOffsetEnding; j+=2) {
+			int index = Char_Byte(Characters.begin(), TempOffset, 2).CastToUint16_BE().variable + 1 + IndexOfSubGeometry;
+			TriangleStripIndexes.push_back(index);
+		}
+
+		IndexOfSubGeometry += TriangleStripIndexes.size();
+		FaceIndexes.push_back(TriangleStripIndexes);
+
 
 	}
 
-	std::vector<Vector3> NormalTable(VerticesTable.size(), Vector3(0, 0, 0));
 
-	for (int i = 0; i < Normals.size(); i++) {
 
+	std::vector<std::vector<int>> FaceList;
+
+
+	for (int j = 0; j < FaceIndexes.size(); j++) {
+
+		for (int k = 0; k < CastStripToFace(FaceIndexes[j]).size(); k++) {
+		    std::vector<int> Temp = CastStripToFace(FaceIndexes[j])[k];
+			FaceList.push_back(Temp);
+		}
 	}
 
 
 
+	
+	UVs = UvTable;
+	Vertices = VerticesTable;
+	Normals = std::vector<Vector3>(VerticesTable.size());
+	Indexes = FaceList;
+
+	
+
+	WriteToFile();
+
+}
+
+
+// what does node at first look like
+void BSPOpen::RecursiveTreeBuilder(BSPNode* Node, int &counter, int &leafcounter)
+{
+	int OffsetToLeftChild = Node->Info.Tenth;
+	int OffsetToRightChild = Node->Info.Eleventh;
+
+
+	// xyz
+	// padding
+	// xyz
+	// padding
+	// parent
+	// left node offset
+	// right node offset
+	// 
+
+	BSP_Node_Header LeftInfo = BSP_Node_Header
+	{
+		LeftInfo.First = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastTo32Float_BE(),
+		LeftInfo.Second = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastTo32Float_BE(),
+		LeftInfo.Third = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastTo32Float_BE(),
+		LeftInfo.Fourth = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_BE().variable,
+		LeftInfo.Fifth = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastTo32Float_BE(),
+		LeftInfo.Sixth = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastTo32Float_BE(),
+		LeftInfo.Seventh = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastTo32Float_BE(),
+		LeftInfo.Eighth = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_BE().variable,
+
+		LeftInfo.Ninth = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_BE().variable,
+		LeftInfo.Tenth = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_BE().variable,
+		LeftInfo.Eleventh = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_BE().variable,
+
+		LeftInfo.Twelfth = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_BE().variable,
+		LeftInfo.Thirteen = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_BE().variable,
+		LeftInfo.Fourteen = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_BE().variable,
+
+		LeftInfo.Fifteen = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_LE().variable,
+
+	    LeftInfo.Sixteen = Char_Byte(Characters.begin(), OffsetToLeftChild, 4).CastToint32_BE().variable
+
+	};
+
+	BSP_Node_Header RightInfo = BSP_Node_Header
+	{
+		RightInfo.First = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastTo32Float_BE(),
+		RightInfo.Second = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastTo32Float_BE(),
+		RightInfo.Third = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastTo32Float_BE(),
+		RightInfo.Fourth = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_BE().variable,
+		RightInfo.Fifth = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastTo32Float_BE(),
+		RightInfo.Sixth = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastTo32Float_BE(),
+		RightInfo.Seventh = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastTo32Float_BE(),
+		RightInfo.Eighth = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_BE().variable,
+
+		RightInfo.Ninth = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_BE().variable,
+		RightInfo.Tenth = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_BE().variable,
+		RightInfo.Eleventh = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_BE().variable,
+
+		RightInfo.Twelfth = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_BE().variable,
+		RightInfo.Thirteen = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_BE().variable,
+		RightInfo.Fourteen = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_BE().variable,
+
+		RightInfo.Fifteen = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_LE().variable,
+
+		RightInfo.Sixteen = Char_Byte(Characters.begin(), OffsetToRightChild, 4).CastToint32_BE().variable
+	};
+
+
+
+
+
+	BSPNode* LeftNode = new BSPNode();
+	BSPNode* RightNode = new BSPNode();
+
+	LeftNode->Info = LeftInfo;
+	RightNode->Info = RightInfo;
+
+	LeftNode->Parent = Node;
+	RightNode->Parent = Node;
+
+
+	// we do this to exclude false info for the last leaf nodes
+	if (!(Node->Info.Tenth <= 0)) {
+		Node->LeftChild = LeftNode;
+	}
+
+	
+
+	if (!(Node->Info.Eleventh <= 0)) {
+		Node->RightChild = RightNode;
+	}
+
+
+	// does node even have a left node we can do down on, and also is it a leaf node via 129
+
+	if (Node->Info.Fifteen == 129) {
+		leafcounter++;
+	}
+
+	if (!(Node->Info.Tenth <= 0) && Node->Info.Fifteen != 129) {
+		counter++;
+		
+		RecursiveTreeBuilder(LeftNode, counter, leafcounter);
+	}
+
+
+
+	// does node even have a right node we can do down on, and also is it a leaf node via 129
+	
+	if (!(Node->Info.Eleventh <= 0) && Node->Info.Fifteen != 129) {
+		counter++;
+
+		RecursiveTreeBuilder(RightNode, counter, leafcounter);
+	}
+	
+}
+
+void BSPOpen::RecursiveLeafFinder(std::vector<BSPNode> &LeafNodes, BSPNode* Node)
+{
+	int IsLeaf = Node->Info.Fifteen;
+
+	if (IsLeaf != 128)
+	{
+		LeafNodes.push_back(*Node);
+		return;
+	
+	}
+
+	if (Node->LeftChild) {
+		RecursiveLeafFinder(LeafNodes, Node->LeftChild);
+	}
+
+	if (Node->RightChild) {
+		RecursiveLeafFinder(LeafNodes, Node->RightChild);
+	}
+
+	
+	
 }
 
 void BSPOpen::ExtraData()
--- Final year final product/Final year final product/Source/BSPOpen.h
+++ Final year final product/Final year final product/Source/BSPOpen.h
@@ -8,6 +8,69 @@
 #include <filesystem>
 
 
+
+struct BSP_Node_Header {
+
+	// x,y and z for the first point, these are big endian float 32s btw
+
+	float First;
+	float Second;
+	float Third;
+
+	// this is just padding, or it denotes the 0x0000008F flag
+	int Fourth;
+
+	// x,y and z for the Second point, these are big endian float 32s btw
+	float Fifth;
+	float Sixth;
+	float Seventh;
+
+	// either just padding OR one of those 0x00008F termination checks, i think these are done to assertain the depth of the tree
+	int Eighth;
+	
+	// the 9th byte is the parent node offset, use that to go back up
+	// the 10th byte, is an offset to the next child LEFT node
+	// the 11th byte, is an offset, to the next child RIGHT node
+
+	int Ninth;
+	int Tenth;
+	int Eleventh;
+
+	// there are 12 more bytes i need to fill out, the second to last or last i forget is either ox80 or 0x81 denoting a final end point
+	// to that node, like a final leaf node, yes i checked
+
+	int Twelfth;
+
+	// 0x0000FFFF
+	int Thirteen;
+
+	// just full of FF's i.e 0xFFFFFFFF
+	int Fourteen;
+
+	// either 0x80 or 0x81, i think 81 is either end leaf node OR denotes when the eactual geometry is done, but i dont know
+	int Fifteen;
+
+	//padding 
+	int Sixteen;
+
+};
+
+
+class BSPNode {
+public:
+
+	BSPNode() {};
+	~BSPNode() {};
+
+public:
+
+	BSPNode* Parent;
+	BSPNode* LeftChild;
+	BSPNode* RightChild;
+
+	BSP_Node_Header Info;
+};
+
 class BSPOpen {
 public:
 	BSPOpen() {}
@@ -23,10 +86,33 @@ public:
 	void Init();
 	void ProcessLines(std::string Line, std::vector<unsigned char>& _Characters);
 	void ReadHeader();
+	void RecursiveTreeBuilder(BSPNode * Node, int& counter, int& leafcounter); 
+    void RecursiveLeafFinder(std::vector<BSPNode>& LeafNodes, BSPNode* Node);
 	void ExtraData();
 	void ConvertToObj();
 	void WriteToFile();
 
+	std::vector<std::vector<int>> CastStripToFace(std::vector<int> _Strip) {
+
+
+		bool Flip = false;
+		std::vector<std::vector<int>> Output;
+
+		for (int i = 0; i < _Strip.size() - 2; i++) {
+			if (Flip) {
+				Output.push_back(std::vector<int>{_Strip[i + 2], _Strip[i + 1], _Strip[i]});
+			}
+
+			else {
+				Output.push_back(std::vector<int>{_Strip[i + 1], _Strip[i + 2], _Strip[i]});
+			}
+
+			Flip = !Flip;
+		}
+
+
+		return Output;
+	}
 
 
 public:
@@ -37,6 +123,8 @@ public:
 
 
 	std::string FilePath;
+
+
 
 
 
@@ -54,6 +142,7 @@ private:
 
 	int AmountOfCharacters;
 
+	int IndexOfSubGeometry = 0;
 
 	std::vector<uint8_t> Geometry;
 
@@ -66,6 +155,6 @@ private:
 	std::vector< std::vector<int>> Indexes;
 
 
-
+	BSPNode* FirstNode = nullptr;
 
 };
